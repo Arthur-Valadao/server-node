04d3686c47bbf49478a1198ec40f00f83e6c2634
Arthur <Arth.valadao@gmail.com>
2024-07-13 22:26:38 -0300

    create routes.js file to facilitate API scaling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 const http = require('http');
-const users = require('./mockup/users');
+const routes = require('./routes');
+const url = require('url');
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`, )
+  const parsedUrl = url.parse(request.url);
+  console.log(parsedUrl);
 
-  if(request.url === '/users' && request.method === 'GET'){
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(users));
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`, )
+
+  const route = routes.find((routeObject)=>(
+    routeObject.endpoint === parsedUrl.pathname && routeObject.method === request.method
+  ));
+
+  if(route){
+    route.handler(request, response);
   }
   else {
     response.writeHead(404, { 'Content-Type': 'application/json' });
-    response.end(`Can not ${request.method} ${request.url}`);
+    response.end(`Can not ${request.method} ${parsedUrl.pathname}`);
   }
+
 })
 
 server.listen(3000, ()=> console.log('Server Started http://localhost:3000'));
