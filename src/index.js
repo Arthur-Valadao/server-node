3324ca13851b5e35184d9ddb6aa8770e12914ad7
Arthur <Arth.valadao@gmail.com>
2024-07-16 17:30:30 -0300

    insert Send method to automate protocol code return

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const http = require('http');
 const routes = require('./routes');
 const { URL } = require('url');
-const { log } = require('console');
 
 const server = http.createServer((request, response) => {
-  const parsedUrl = new URL(`http://localhost:3000${request.url}`)
-  console.log(Object.fromEntries(parsedUrl.searchParams));
+  // const parsedUrl = url.parse(request.url, true)
+  const parsedUrl =  new URL(`http://localhost:3000${request.url}`)
 
   let { pathname } = parsedUrl;
+  let id = null;
 
   //split do endpoint e remoção de string vazia
   const splitEndPoint = pathname.split('/').filter(Boolean);
-  console.log(splitEndPoint);
+
+  if (splitEndPoint.length = 1){
+    pathname = `/${splitEndPoint[0]}/:id`;
+    id = splitEndPoint[1];
+  }
 
   console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`, )
 
@@ -21,6 +25,12 @@ const server = http.createServer((request, response) => {
 
   if(route){
     request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ body }))
+    }
 
     route.handler(request, response);
   }
